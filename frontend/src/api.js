344b9a4afb3e656559d311f8610f79937a9f7f51
Tulasi Shukla <tulsishuklag@gmail.com>
2025-11-02 14:20:50 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,15 @@
+// import axios from "axios";
+
+
+
+// export const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || "https://porfolio-maker-2.onrender.com/portfolios",
+//   // add render url here
+//   timeout: 10000
+// });
 import axios from "axios";
 
-
-
 export const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://porfolio-maker-2.onrender.com/portfolios",
-  // add render url here
+  baseURL: import.meta.env.VITE_API_URL || "https://porfolio-maker-2.onrender.com/api",
   timeout: 10000
 });
